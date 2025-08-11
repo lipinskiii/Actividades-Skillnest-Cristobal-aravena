93673377c10e1194586802bc12f91b7414fae23e
lipinskiii <cris.arav.lipinski@outlook.cl>
2025-08-11 05:15:36 -0400

    auto

---
+++ T5Onlick/script.js
@@ -0,0 +1,32 @@
+
+document.getElementById('btnSesion').addEventListener('click', function() {
+    if (this.textContent === 'Iniciar sesión') {
+        this.textContent = 'Cerrar sesión';
+    } else {
+        this.textContent = 'Iniciar sesión';
+    }
+});
+
+
+document.querySelector('.Agregardefinicion').addEventListener('click', function() {
+    this.style.display = 'none'; 
+});
+
+
+function megusta(titulo) {
+    
+    alert(`Te gustó "${titulo}"`);
+    
+    
+    const boton = event.target;
+    
+    
+    let texto = boton.textContent;
+    let numeroLikes = parseInt(texto.match(/\d+/)[0]);
+    
+    
+    numeroLikes++;
+    
+    
+    boton.textContent = `${numeroLikes} me gusta`;
+}
