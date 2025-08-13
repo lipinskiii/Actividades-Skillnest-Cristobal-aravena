84bacf0c6d8494a2fa303c85534dacf94dc8b5eb
lipinskiii <cris.arav.lipinski@outlook.cl>
2025-08-13 16:50:41 -0400

    auto

---
+++ T5paginadeperfil/Javascri.js
@@ -0,0 +1,20 @@
+let botonEliminar = document.querySelectorAll(".boton2");
+
+for(let i = 0; i < botonEliminar.length; i ++){
+    botonEliminar[i].addEventListener("click", function(event){
+        // Remover el elemento contenedor
+        event.target.closest(".Solicitudesconecc").remove();
+        // this.closest(".card-list-item").remove();
+
+        // Actualizar el número de conexiones en la página
+        let connectionRequestNumber = document.querySelector("#connectionRequestNumber");
+        let valorActual = connectionRequestNumber.textContent;
+        valorActual --;
+        connectionRequestNumber.textContent = valorActual;
+        let TotalConnections = document.querySelector("#totalConnections")
+        let Total = TotalConnections.textContent;
+        Total ++;
+        TotalConnections.textContent = Total;
+        
+    });
+}
