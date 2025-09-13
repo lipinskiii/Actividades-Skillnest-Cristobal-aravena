13d6b1e7062008064f4c4c24268681c36a8881fa
lipinskiii <cris.arav.lipinski@outlook.cl>
2025-09-13 03:31:15 -0300

    auto

---
+++ Canciones/mostrar_canciones/src/main/java/com/cristobal_aravena/mostrar_canciones/MostrarCancionesApplication.java
@@ -0,0 +1,13 @@
+package com.cristobal_aravena.mostrar_canciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostrarCancionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostrarCancionesApplication.class, args);
+	}
+
+}
---
+++ Canciones/mostrar_canciones/src/test/java/com/cristobal_aravena/mostrar_canciones/MostrarCancionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristobal_aravena.mostrar_canciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MostrarCancionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
