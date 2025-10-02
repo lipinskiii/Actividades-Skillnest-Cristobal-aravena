fb3c38b01e2da1f5de3b66cbc0f25723344da73d
lipinskiii <cris.arav.lipinski@outlook.cl>
2025-10-01 23:35:59 -0300

    auto

--- Ultimacorejava/login_register/src/main/java/com/cristobal_aravena/login_register/LoginRegisterApplication.java
+++ Ultimacorejava/login_register/src/main/java/com/cristobal_aravena/login_register/LoginRegisterApplication.java
@@ -2,6 +2,10 @@ package com.cristobal_aravena.login_register;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class LoginRegisterApplication {
@@ -9,5 +13,11 @@ public class LoginRegisterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LoginRegisterApplication.class, args);
 	}
+	 @Bean
+    public FilterRegistrationBean<HiddenHttpMethodFilter> hiddenHttpMethodFilter() {
+        FilterRegistrationBean<HiddenHttpMethodFilter> filterRegBean = new FilterRegistrationBean<>(new HiddenHttpMethodFilter());
+        filterRegBean.addUrlPatterns("/*");
+        return filterRegBean;
+    }
 
 }
