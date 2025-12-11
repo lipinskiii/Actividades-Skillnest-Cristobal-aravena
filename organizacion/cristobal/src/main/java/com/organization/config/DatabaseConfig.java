a56f4534b849661ce636cfa688a5e1fa750a704a
Lipinski <164947710+lipinskiii@users.noreply.github.com>
2025-12-10 23:12:21 -0300

    Update database password in DatabaseConfig

--- organizacion/cristobal/src/main/java/com/organization/config/DatabaseConfig.java
+++ organizacion/cristobal/src/main/java/com/organization/config/DatabaseConfig.java
@@ -8,7 +8,7 @@ public class DatabaseConfig {
 
     private static final String URL = "jdbc:mysql://localhost:3306/organizador?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "tu_password";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
